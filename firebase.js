cbd9a35a9e84cebd0c6461267ce7b6ac0cf76e94
ebswing9 <ebswing9@gmail.com>
2026-08-15 16:18:11 +0900

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -12,11 +12,11 @@
  * 나중에 값이 바뀌어도 여기 한 곳만 고치면 됩니다.
  */
 const firebaseConfig = {
-  apiKey: "AIzaSyCp3vLQM0aKX7s1ArE2YD3BsLNHcbQQJFE",
-  authDomain: "classboard-58841.firebaseapp.com",
-  databaseURL: "https://classboard-58841-default-rtdb.firebaseio.com",
-  projectId: "classboard-58841",
-  storageBucket: "classboard-58841.firebasestorage.app",
-  messagingSenderId: "961369750358",
-  appId: "1:961369750358:web:86a2d7a9c25f1e6204b4f7"
+  apiKey: "AIzaSyAKlIm_ZDD4ICyROlSskTKP5fpSixkSeIU",
+  authDomain: "todaysquiz-5167d.firebaseapp.com",
+  databaseURL: "https://todaysquiz-5167d-default-rtdb.firebaseio.com",
+  projectId: "todaysquiz-5167d",
+  storageBucket: "todaysquiz-5167d.firebasestorage.app",
+  messagingSenderId: "582156880416",
+  appId: "1:582156880416:web:3f750531895cdc5f9a4a45"
 };
